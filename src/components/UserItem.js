c86a5131a07d1d314bb39c77ecd511f38e34e3bb
jiangg1 <gao.jiang@news.com.au>
2018-07-24 22:33:32 +1000

    readme

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import UserName from './UserName';
 import UserInfo from './UserInfo';
+import ApiService from '../ApiService';
 import { Link } from 'react-router-dom';
 import { ListGroup } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-const UserItem = (props) => {
-    let first_name, last_name, id, todo_count, info;
-    ({id, first_name, last_name, todo_count, ...info} = props);
-    return (
-        <li className="list-group-item">
-            <div className="card">
-                <UserName first_name={first_name} last_name={last_name} />
-                <UserInfo {...info} />
-                
-                <Link className='navbar navbar-light bg-light' to={`/todos/${id}`}>View Todos ({todo_count})</Link>
-            </div>
-        </li>
-    );
+class UserItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: []
+        };
+    }
+
+    async componentDidMount() {
+        const userId = parseInt(this.props.id, 10);
+        try {
+            const todos = await ApiService.getTodos({userId});
+            this.setState({todos});
+        } catch (e) {
+            console.error(`An error ${e.message} occured while loading tasks for user ${userId}`);
+        }
+    }
+
+    render() {
+        return (
+            <li className="list-group-item">
+                <div className="card">
+                    <UserName first_name={this.props.first_name} last_name={this.props.last_name} />
+                    <UserInfo {...this.props} />
+                    <Link className='navbar navbar-light bg-light' to={`/todos/${this.props.id}`}>View Todos ({this.props.todo_count})</Link>
+                </div>
+            </li>
+        );
+    }
 
 };
 
